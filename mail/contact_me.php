23a3d5e06a621b4fc76f375f47f3b881b104ab18
Marco Souza <Marco Souza>
2017-04-14 14:00:46 -0300

    Alteração de e-mail de destino

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -13,7 +13,7 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = 'contato@naturalparkresidencial.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'atendimento@naturalparkresidencial.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contato via Web Site:  $name";
 $email_body = "Você recebeu uma nova mensagem do formulário de contato do Web Site.\n\n"."Detalhes da mensagem:\n\nNome: $name\n\nE-mail: $email_address\n\nMensagem:\n$message";
 $headers = "De: noreply@naturalparkresidencial.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
